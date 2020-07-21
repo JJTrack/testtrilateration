4673687dd61beff511f136ed910955fd9802a7bb
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-07-22 09:07:46 +1200

    refactored to draw function for nodes and beacons

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const node3 = {
 }
 
 const beacon = {
-    x: 6,
-    y: 10,
+    x: 7,
+    y: 16,
     z: 0
 }
 
@@ -28,15 +28,26 @@ let ctx = c1.getContext("2d");
 let c2 = document.getElementById("myCanvasTwo");
 let ctx2 = c2.getContext("2d");
 
-// Draw nodes
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(node1.x * 20, node1.y * 20, 20, 20);
-ctx.fillRect(node2.x * 20, node2.y * 20, 20, 20);
-ctx.fillRect(node3.x * 20, node3.y * 20, 20, 20);
+let nodes = [node1, node2, node3];
 
-// Draw beacon
-ctx.fillStyle = "#000000";
-ctx.fillRect(beacon.x * 20, beacon.y * 20, 20, 20);
+
+let drawNodesAndBeacons = (nodes) => {
+
+    nodes.forEach(node => {
+        // Draw nodes
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(node.x * 20, node.y * 20, 20, 20); 
+    });
+
+    // Draw beacon
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(beacon.x * 20, beacon.y * 20, 20, 20);
+
+
+}
+
+drawNodesAndBeacons(nodes);
+
 
 
 // Just using Three Cartesian dimensions, three measured slant ranges
@@ -48,7 +59,7 @@ let r1 = Math.sqrt(Math.pow(beacon.x - node1.x, 2) + Math.pow(beacon.y - node1.y
 let U = Math.sqrt(Math.pow(node2.x - node1.x, 2) + Math.pow(node2.y - node1.y, 2));
 let r2 = Math.sqrt(Math.pow(node2.x - beacon.x, 2) + Math.pow(node2.y - beacon.y, 2) +  Math.pow(node2.z - beacon.z, 2));
 let r3 = Math.sqrt(Math.pow(node3.x - beacon.x, 2) + Math.pow(node3.y - beacon.y, 2) +  Math.pow(node3.z - beacon.z, 2));
-let rx = 19;
+let rx = node3.x - node1.y;
 let ry = node2.y - node1.y;
 
 let calculatedX = ((Math.pow(r1, 2) - Math.pow(r3, 2)) + Math.pow(rx, 2)) / (rx*2);
